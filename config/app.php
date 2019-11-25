23c0d6fa979294ad5cafbd2d765d71cdb36723d9
Harmen Janssen <harmen@grrr.nl>
2019-11-25 15:06:13 +0100

    Add Sentry service provider

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Sentry\Laravel\ServiceProvider::class,
 
         /*
          * Application Service Providers...
